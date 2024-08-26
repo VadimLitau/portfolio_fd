16aa9fb3d934c9cdccaa87905376c7f2594dd78a
VadimLitau <vadimlitau@yandex.ru>
2024-08-26 19:41:51 +0500

    new project

--- src/helpers/projectsData.ts
+++ src/helpers/projectsData.ts
@@ -2,6 +2,7 @@ import belaela from "../img/projects/belaela.svg";
 import belaelaBig from "../img/projects/belaela_big.png";
 import tere from "../img/projects/tere.png";
 import tereBig from "../img/projects/tere_big.png";
+import toDo from "../img/projects/ToDoApp.png";
 export const projectsData = [
 	{
 		img: belaela,
@@ -16,7 +17,15 @@ export const projectsData = [
 		img_big: tereBig,
 		title: "Tere",
 		gitHubLink: "https://github.com/VadimLitau/Tere",
-		skils: "html, scss, gulp",
+		skils: "html, sass, gulp",
 		id: 2,
+	},
+	{
+		img: toDo,
+		img_big: toDo,
+		title: "To Do App",
+		gitHubLink: "https://github.com/VadimLitau/ToDo_TS",
+		skils: "React, TS, SASS, Suztand",
+		id: 3,
 	},
 ];
